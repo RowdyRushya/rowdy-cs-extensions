3bb61ddafc51889f5d4d9ddabfd0ccad9ee789ff
rushi-chavan <rushikeshchavan9887@gmail.com>
2024-02-16 02:07:12 -0800

    New Extension: DesiXFlix

---
+++ DesiXFlix/src/main/kotlin/com/rowdyCSExtensions/Extractors.kt
@@ -0,0 +1,7 @@
+package com.rowdyCSExtensions
+
+import com.lagradost.cloudstream3.extractors.DoodLaExtractor
+
+class D0000dExtractor : DoodLaExtractor() {
+    override var mainUrl = "https://d0000d.com"
+}
